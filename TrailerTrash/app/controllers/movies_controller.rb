eddaea3245c48522951e49c4ae240dc1c1c8d015
Julia Rose <julmrose@gmail.com>
2015-04-17 15:18:30 -0400

    added image search and moved logic to models

--- TrailerTrash/app/controllers/movies_controller.rb
+++ TrailerTrash/app/controllers/movies_controller.rb
@@ -3,25 +3,17 @@ class MoviesController < ApplicationController
 	def index
 		if params[:query]
 			query = params[:query]
-			array = query.split
-			plus_query = array.join("+")
-			@response = HTTParty.get("http://www.omdbapi.com/?t=#{plus_query}&y=&plot=short&r=json").parsed_response
-			@title = @response["Title"]
-			@year = @response["Year"]
-			@poster_url = @response["Poster"]
-			@plot = @response["Plot"]
-			@movie = Movie.new(title: @title, year: @year, poster_url: @poster_url, plot: @plot)
-			if @movie.save
-				@movie
-			else
-				@movie = Movie.find_by(title: @title)
-			end
+			@array = Movie.find(query)
+			@movie = @array[0]
+			@actors = @array[1]
+
+			@movie.get_actors(@actors)
+			
 		end
-
-
 	end
 
 	def show
+		@movie = Movie.find(params[:id])
 
 	end
 
--- TrailerTrash/app/models/actor.rb
+++ TrailerTrash/app/models/actor.rb
@@ -1,4 +1,5 @@
 class Actor < ActiveRecord::Base
 	belongs_to :movie
+	validates_uniqueness_of :name, :scope => [:movie_id]
 
 end
