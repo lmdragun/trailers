c61b372f2a983c18d57b83c8f81968a0c98a835a
Julia Rose <julmrose@gmail.com>
2015-04-17 12:00:50 -0400

    adding pry for working in movies_controller

--- TrailerTrash/app/controllers/movies_controller.rb
+++ TrailerTrash/app/controllers/movies_controller.rb
@@ -2,7 +2,11 @@ class MoviesController < ApplicationController
 
 	def index
 		query = params[:query]
+		array = query.split
+		plus_query = array.join("+")
+		@response = HTTParty.get("http://www.omdbapi.com/?t=#{plus_query}&y=&plot=short&r=json").parsed_response.inspect
 
+		binding.pry
 	end
 
 	def show
